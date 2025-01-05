337f0d51583a9e7cb05e45edca227a2eea5a09aa
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-01-06 00:23:20 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$d5c5h&vkazayd$nevzv2%xrexgv9jv0rauzx(+ue^-0)##&ly'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app.onrender.com']
 
 
 # Application definition
